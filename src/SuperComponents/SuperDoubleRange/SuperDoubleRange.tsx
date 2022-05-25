50356d9b1f50de4e28689a115682516048666045
Ivan0195 <iihnatsenka95@gmail.com>
2022-05-25 20:21:56 +0300

    first commit

---
+++ src/SuperComponents/SuperDoubleRange/SuperDoubleRange.tsx
@@ -0,0 +1,72 @@
+import s from '../c7-SuperRange/SuperRange.module.css'
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+
+type SuperDoubleRangePropsType = DefaultInputPropsType & {
+    onChangeRange?: (value: number) => void
+    onChangeRange2?: (value: number) => void
+    minValueChanger: string | number
+    maxValueChanger: string | number
+    numberContentMin: string | number
+    numberContentMax: string | number
+}
+
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+    {
+        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        onChange, onChangeRange, onChangeRange2,
+        className,
+        maxValueChanger, minValueChanger, numberContentMin, numberContentMax,
+        ...restProps// все остальные пропсы попадут в объект restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e) // сохраняем старую функциональность
+
+        onChangeRange && onChangeRange(+e.currentTarget.value)
+    }
+
+    const onChangeCallback2 = (e: ChangeEvent<HTMLInputElement>) => {
+
+        onChange && onChange(e) // сохраняем старую функциональность
+
+        onChangeRange2 && onChangeRange2(+e.currentTarget.value)
+    }
+
+    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+
+    return (
+        <>
+            <div className={s.numberValue}>
+                <div className={s.minValue}>{numberContentMin}</div>
+                <div className={s.maxValue}>{numberContentMax}</div>
+            </div>
+            <div className={s.slider}>
+                <div className={s.progress}></div>
+            </div>
+            <span className={s.range_input}>
+            <input
+                type={'range'}
+                onChange={onChangeCallback}
+                className={finalRangeClassName}
+                min={'0'} max={'100'}
+                value={minValueChanger}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+            <input
+                type={'range'}
+                onChange={onChangeCallback2}
+                className={finalRangeClassName}
+                value={maxValueChanger}
+                min={'0'} max={'100'}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+        </span>
+        </>
+    )
+}
+
+
+export default SuperDoubleRange
---
+++ src/SuperComponents/SuperRange/SuperRange.tsx
@@ -0,0 +1,49 @@
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import s from './SuperRange.module.css'
+
+// тип пропсов обычного инпута
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+// здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
+// (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
+type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
+    onChangeRange?: (value: number) => void
+    minValueChanger: number | string
+};
+
+const SuperRange: React.FC<SuperRangePropsType> = (
+    {
+        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        onChange, onChangeRange,
+        className, minValueChanger,
+        ...restProps// все остальные пропсы попадут в объект restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e) // сохраняем старую функциональность
+
+        onChangeRange && onChangeRange(+e.currentTarget.value)
+    }
+
+    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+
+    return (
+        <div>
+            <div className={s.slider}>
+                <div className={s.progress}></div>
+            </div>
+            <span className={s.range_input}>
+            <input
+                type={'range'}
+                onChange={onChangeCallback}
+                className={finalRangeClassName}
+                value={minValueChanger}
+                min={'0'} max={'100'}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+        </span>
+        </div>
+    )
+}
+
+export default SuperRange
