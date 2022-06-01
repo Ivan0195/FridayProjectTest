bc69de8d4b306f39ef13d897a904709c0b49c414
Ivan0195 <iihnatsenka95@gmail.com>
2022-06-01 17:01:22 +0300

    AddedResetPasswordAPI&ResetPasswordReducer

---
+++ src/bll/reset-password-reducer.ts
@@ -0,0 +1,52 @@
+import {AppDispatch} from "./store";
+import {setAppStatusAC} from "./common-app-reducer";
+import {resetPasswordAPI} from "../api/reset-password-api";
+
+const resetPasswordInitialState: resetPasswordInitialStateType = {
+    error: null,
+    tokenIsSent: false
+}
+export type resetPasswordInitialStateType = {
+    error: null | string,
+    tokenIsSent: boolean
+}
+
+export type setErrorActionType = ReturnType<typeof setAppErrorAC>;
+export type setIsInitializedActionType = ReturnType<typeof setTokenIsSentAC>;
+
+export type ResetPasswordActionsType = setErrorActionType | setIsInitializedActionType
+
+export const setAppErrorAC = (error: string | null) => ({ type: 'RESET-PASSWORD/SET-ERROR', error } as const)
+export const setTokenIsSentAC = (tokenIsSent: boolean) => ({type: 'RESET-PASSWORD/TOKEN-IS-SENT',tokenIsSent} as const)
+
+const resetPasswordReducer = (state = resetPasswordInitialState, action: ResetPasswordActionsType) => {
+    switch (action.type) {
+        case "RESET-PASSWORD/SET-ERROR":
+            return { ...state, error: action.error }
+        case "RESET-PASSWORD/TOKEN-IS-SENT":
+            return { ...state, tokenIsSent: action.tokenIsSent }
+        default: return { ...state };
+    }
+
+}
+
+const from = "test-front-admin<ai73a@yandex.by>"
+const message = `<div style="background-color: lime; padding: 15px"> 
+go to reset your password: 
+<a href='https://Ivan0195.github.io/FridayProject#/newPassword/$token$'>
+link</a></div>`
+
+export const sendTokenTC = (email: string) => (dispatch: AppDispatch) => {
+    dispatch(setAppStatusAC("pending"))
+    resetPasswordAPI.resetPassword(email, from, message)
+        .then(() => {
+            dispatch(setAppStatusAC("successful"))
+            dispatch(setTokenIsSentAC(true))
+        })
+        .catch((error) => {
+            dispatch(setAppStatusAC("failed"))
+            dispatch(setAppErrorAC(error.response.data.error))
+        })
+}
+
+export default resetPasswordReducer;
--- src/bll/store.ts
+++ src/bll/store.ts
@@ -5,17 +5,19 @@ import {registrationReducer} from "./registration-reducer";
 import {profileReducer} from "./profile-reducer";
 import {useDispatch} from "react-redux";
 import {appStatusReducer, SetAppStatusActionType} from "./common-app-reducer";
+import resetPasswordReducer, {ResetPasswordActionsType} from "./reset-password-reducer";
 
 const rootReducer = combineReducers({
     login: loginReducer,
     registration: registrationReducer,
     profile: profileReducer,
     appStatus: appStatusReducer,
+    resetPassword: resetPasswordReducer,
 })
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
-export type AppActionType = LoginActionsType | SetAppStatusActionType // сюда нужно дописать общий тип для вашего редьюсера
+export type AppActionType = LoginActionsType | SetAppStatusActionType | ResetPasswordActionsType // сюда нужно дописать общий тип для вашего редьюсера
 export type AppDispatch = typeof store.dispatch;
 export type TypedDispatch = ThunkDispatch<AppRootStateType, any, AppActionType>;
 
