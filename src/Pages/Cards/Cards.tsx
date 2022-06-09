0f0d1fbae7e80890b308e0c89d36f3e989640f6b
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-06-09 21:31:25 +0300

    paginator style

--- src/Pages/Cards/Cards.tsx
+++ src/Pages/Cards/Cards.tsx
@@ -1,64 +1,67 @@
-import { Table } from '../../components/Table';
-import { SortableTableHeader } from '../../components/Table/elements/SortableTableHeader';
-import { CardsResponseType, CardType } from '../../types/responseTypes';
-import { useParams } from 'react-router-dom';
-import { Rate } from '../../components/common/Rate';
-import React, { useEffect } from 'react';
-import { AppRootStateType, useTypedDispatch } from '../../bll/store';
-import { fetchCard, getCards, setCards } from '../../bll/packs-reducer';
-import { useSelector } from 'react-redux';
-import { Search } from '../../components/common/SearchBlock/Search';
-import { Pagination } from '../../components/common/Pagination/Pagination';
+import {Table} from '../../components/Table';
+import {SortableTableHeader} from '../../components/Table/elements/SortableTableHeader';
+import {CardsResponseType, CardType} from '../../types/responseTypes';
+import {useParams} from 'react-router-dom';
+import {Rate} from '../../components/common/Rate';
+import React, {useEffect} from 'react';
+import {AppRootStateType, useTypedDispatch} from '../../bll/store';
+import {fetchCard, getCards, setCards} from '../../bll/packs-reducer';
+import {useSelector} from 'react-redux';
+import {Search} from '../../components/common/SearchBlock/Search';
+import {Pagination} from '../../components/common/Pagination/Pagination';
 
 const columns = [
-  {
-    id: 'question',
-    value: <SortableTableHeader onClick={console.log}>Question</SortableTableHeader>,
-  },
-  {
-    id: 'answer',
-    value: <SortableTableHeader onClick={console.log}>Answer</SortableTableHeader>,
-  },
-  {
-    id: 'updated',
-    value: <SortableTableHeader onClick={console.log}>Last Updated</SortableTableHeader>,
-  },
-  {
-    id: 'user_name',
-    value: <SortableTableHeader onClick={console.log}>Grade</SortableTableHeader>,
-    render: (card: CardType) => {
-      return (
-        <Rate value={Math.floor(card.grade)} />
-      );
+    {
+        id: 'question',
+        value: <SortableTableHeader onClick={console.log}>Question</SortableTableHeader>,
     },
-  },
+    {
+        id: 'answer',
+        value: <SortableTableHeader onClick={console.log}>Answer</SortableTableHeader>,
+    },
+    {
+        id: 'updated',
+        value: <SortableTableHeader onClick={console.log}>Last Updated</SortableTableHeader>,
+    },
+    {
+        id: 'user_name',
+        value: <SortableTableHeader onClick={console.log}>Grade</SortableTableHeader>,
+        render: (card: CardType) => {
+            return (
+                <Rate value={Math.floor(card.grade)}/>
+            );
+        },
+    },
 ];
 
 export const Cards = () => {
-  const dispatch = useTypedDispatch();
-  const cards = useSelector<AppRootStateType, CardsResponseType | null>(getCards);
-  const { id } = useParams<{ id: string }>();
+    const dispatch = useTypedDispatch();
+    const cards = useSelector<AppRootStateType, CardsResponseType | null>(getCards);
+    const {id} = useParams<{ id: string }>();
 
-  useEffect(() => {
-    if (id) {
-      dispatch(fetchCard(id));
-    }
+    useEffect(() => {
+        if (id) {
+            dispatch(fetchCard(id));
+        }
 
-    return () => {
-      dispatch(setCards(null));
-    };
-  }, [dispatch, id]);
+        return () => {
+            dispatch(setCards(null));
+        };
+    }, [dispatch, id]);
 
-  return (
-    <>
-      <Search onChange={() => {}}/>
-      <Table columns={columns} items={cards ? cards.cards : []} itemRowKey="_id" />
-      <Pagination currentPage={1}
-                  itemsOnPageCount={5}
-                  totalItemsCount={45}
-                  onPageChanged={() => {}}
-                  onChangeItemsOnPageCount={() => {}}
-      />
-    </>
-  );
+    return (
+        <>
+            <Search onChange={() => {
+            }}/>
+            <Table columns={columns} items={cards ? cards.cards : []} itemRowKey="_id"/>
+            <Pagination currentPage={1}
+                        itemsOnPageCount={5}
+                        totalItemsCount={45}
+                        onPageChanged={() => {
+                        }}
+                        onChangeItemsOnPageCount={() => {
+                        }}
+            />
+        </>
+    );
 };
