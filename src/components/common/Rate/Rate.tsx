d9cafbcd0187cd6122290f5353b1f0cc5a1a9c57
Nikon1624 <denis.nikonov1@simbirsoft.com>
2022-06-09 23:19:25 +0500

    Rate

---
+++ src/components/common/Rate/Rate.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import cn from 'classnames';
+import { ReactComponent as StarIcon } from '../../../assets/images/star.svg';
+import styles from './Rate.module.css';
+
+export type RatePropsType = {
+  value: number | string;
+  onChange?: (value: number) => void;
+};
+
+const maxRateValue = 5;
+
+
+export const Rate: React.FC<RatePropsType> = ({ value, onChange }) => {
+  return (
+    <div>
+      {
+        [...Array(maxRateValue)].map((item, index) => {
+          return (
+            <StarIcon
+              key={index}
+              className={cn(styles.star, {
+                [styles.checked]: index <= Number(value) - 1,
+                [styles.clickable]: !!onChange,
+              })}
+              onClick={onChange ? () => onChange(index + 1) : undefined}
+            />
+          );
+        })
+      }
+    </div>
+  );
+};
